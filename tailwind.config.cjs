18352c62964986d18acdcfd9354d943f536ea8c0
luco17 <lucodonoghue@gmail.com>
2023-06-30 06:57:29 +0100

    hero image changes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,9 +9,21 @@ module.exports = {
         rafred: "#cf0a2c",
         govblue: "#1d70b8",
       },
+      keyframes: {
+        fadeUp: {
+          "0%": { transform: "translateY(20px)" },
+          "100%": { transform: "translateY(0)" },
+        },
+        fadeOpacity: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+      animation: {
+        fadeUp: "fadeUp 0.8s ease forwards 0.5s",
+        fadeOpacity: "fadeOpacity 0.4s ease forwards 0.5s",
+      },
     },
   },
-  plugins: [ 
-    require('@tailwindcss/typography')
-  ],
+  plugins: [require("@tailwindcss/typography")],
 };
